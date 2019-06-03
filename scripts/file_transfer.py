57d6566b7cfe3df3cb4e0118b8f909bd6c9114c5
BEACON <nuphase@beaglebone.localdomain>
2019-06-03 19:39:48 +0000

    Changed the name of the transfer file and tested it out with some small files. Might want to add checks in the future so it doesn't overload the storage on sundog.

--- scripts/file_transfer.py
+++ scripts/file_transfer.py
@@ -2,25 +2,21 @@ import time
 import os
 import sys
 import glob
-from scp import SCPClient
 from paramiko import SSHClient
+from scp import SCPClient
 
 # Define progress callback that prints the current percentage completed for the file
 def progress(filename, size, sent):
+    if type(filename) == bytes:
+        filename = filename.decode()
     sys.stdout.write("%s\'s progress: %.2f%%   \r" % (filename, float(sent)/float(size)*100) )
 
-
-
-scp.put('test.txt', '~/test.txt')
-# Should now be printing the current progress of your put function.
-
-scp.close()
 
 if __name__ == "__main__":
     input_dir = '/home/nuphase/BEACON-SDR/captures/' #On Beagle (Where the script should be run.
     output_dir = '/home/dsouthall/BEACON-SDR/captures/' #On Sundog
     wait_time = 10 #s
-    
+    extensions = ['*.bin','*.met']
     call_time = time.time()
     
     ssh = SSHClient()
@@ -30,7 +26,9 @@ if __name__ == "__main__":
     # SCPCLient takes a paramiko transport and progress callback as its arguments.
     with SCPClient(ssh.get_transport(), progress=progress) as scp:
         while True:
-            files = glob.glob(input_dir + '*.csv')
+            files = []
+            for ext in extensions:
+                files.extend(glob.glob(input_dir + ext))
             for f in files:
                 f = f.replace('\\','/') # To handle windows paths.
                 t = os.path.getmtime(f)
@@ -41,6 +39,9 @@ if __name__ == "__main__":
                     print(f_new)
                     #os.rename(f,f_new) #Might want to do an scp if doing between pcs?
                     scp.put(f, f_new)
+                    print('\n')
+                    os.remove(f)
+                    print('%s removed from local storage.\n'%f)
             call_time = time.time()
             time.sleep(wait_time)
-    
+        scp.close()
