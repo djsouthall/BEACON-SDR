2f20b55f97913fce969b787e71eacfb7749a3d67
Dan Southall <dsouthall@uchicago.edu>
2019-06-11 15:18:05 +0000

    Added a script to run the rtl_power_fftw function.

---
+++ scripts/capture_data.py
@@ -0,0 +1,185 @@
+import subprocess
+import sys
+import os
+import time
+
+def rtlPowerFFTW(baseline=None, bins=None, buffers=None, continuous=False, device=None, \
+                elapsed=None, freq=[30e6, 100e6], gain=None, linear=False, matrix='capture', \
+                n=None, overlap=None, ppm=None, quiet=False, rate=None, buffer_size=None, \
+                strict_time=None, time=None, window=None, ignore_rest=False):
+    '''
+    This is a wrapper function for submitting fftw_power in python.  This will just submit the code
+    from https://github.com/AD-Vega/rtl-power-fftw.
+
+    Note that I have not actually setup ALL of the kwargs below as I have not used them and didn't
+    spend the time to figure out what they do. 
+
+    Parameters
+    ----------
+    baseline : int, optional
+        Subtract baseline, read baseline data from a file or from standard input. See BASELINE AND 
+        WINDOW FUNCTION DATA below for format and further considerations.
+        Will append command with -B <file|->, --baseline <file|->
+        Default is None.
+    bins : int, optional
+        Number of bins in the FFT spectrum (must be an even number).  Will append command with
+        -b <bins in FFT spectrum>
+        Default is None.
+    buffers : int, optional
+        Number of read buffers: don't touch unless running out of memory.  Default is None.
+    continuous : bool, optional
+        Repeat the same measurement endlessly. The spectra are written sequentially to the output 
+        and a header is appended before each measurement (as usual).  Default is False.
+    device : ?, optional 
+        RTL-SDR device index of the device used for the measurement.  Default is None.
+    elapsed : str, optional
+        Allows to specify the recording duration in hours, sec, mins.  This will be appended to the
+        command.  Formatting should resemble '5m' for 5 minutes.  Default is None.
+    freq : list of ints, optional
+        Center frequency of the receiver or the frequency range to scan. Given in Hz. Frequency 
+        range consists of lower and upper bound, with the smaller frequency being used as the lower 
+        bound and the larger as the upper.  Default is [30e6, 100e6].
+    gain : int, optional
+        Receiver gain, expressed in tenths of a decibel (e.g., 100 means 10 dB).  Default is None.
+    linear : bool, optional
+        Calculate linear power values instead of logarithmic.  Default is False.
+    matrix : str, optional
+        Specify a file name (no extension) and use it to store the power values in binary format 
+        within a .bin file plus a metadata text file with .met extension.  Default is 'capture'.
+    n : int
+        Number of spectra to average (incompatible with -t).  Default is None.
+    overlap : int
+        Define lower boundary for overlap when frequency hopping (otherwise meaningless).  This sets
+        the desired lower bound for overlap in percentage of bandwidth.  Default is None.
+    ppm : int, optional
+        Correct for the oscillator error of RTL-SDR device. The correction should be given in ppm.
+        Default is None.
+    quiet : bool
+        Limit verbosity. Allows the various printouts to happen only the first time and not on every 
+        scan.  Default is False
+    rate : int, optional
+        Sample rate of the receiver in Hz.  Default is None.
+    buffer_size : int, optional
+        Size of the read buffers (leave it as it is unless you know what you are doing).  Default is
+        None.
+    strict_time : bool, optional
+        End measurement when the time set with time option is up, regardless of the number of 
+        gathered samples.  Default is False. 
+    time : str, optional
+        Integration time (incompatible with -n). This is an effective integration time; see 
+        INTEGRATION TIME below for more info (in short, the measurement might take longer than 
+        that).  Same timing as elapsed.  Default is None.
+    window : ?, optional
+        Use a window function, read data from a file or from standard input. See BASELINE AND WINDOW 
+        FUNCTION DATA below for format and further considerations.  Default is None.
+    ignore_rest : bool, optional
+        Ignore the rest of the labeled arguments following this flag.  Default is False.
+
+    Returns
+    -------
+    output : str
+        The std output value from running the command.
+    error : str
+        The std error output value from running the command.
+
+    See Also
+    --------
+    https://github.com/AD-Vega/rtl-power-fftw/blob/master/doc/rtl_power_fftw.1.md
+    '''
+    command = 'rtl_power_fftw'
+    if baseline is not None:
+        command += ' --baseline %i'%int(baseline)
+    if bins is not None:
+        command += ' --bins %i'%int(bins)
+    if buffers is not None:
+        command += ' --bins %i'%int(bins)
+    if continuous == True:
+        command += ' --continue'
+    if device is not None:
+        command += ' --device %i'%int(device)
+    if elapsed is not None:
+        command += ' --elapsed %s'%elapsed
+    if type(freq) is not list:
+        print('freq must be a list.  Breaking.')
+        return
+    elif len(freq) == 1:
+        command += ' --freq %i'%int(freq[0])
+    elif len(freq) == 2:
+        command += ' --freq %i:%i'%(int(min(freq)),int(max(freq)))
+    else:
+        print('freq must be a list of size 1 or 2.  Breaking.')
+        return
+    if gain is not None:
+        command += ' --gain %i'%int(gain)
+    if linear == True:
+        command += ' --linear'
+    if matrix is not None:
+        command += ' --matrix %s'%str(matrix)
+    if n is not None:
+        command += ' --n %i'%int(n)
+    if overlap is not None:
+        command += ' --overlap %i'%int(overlap)
+    if ppm is not None:
+        command += ' --ppm %i'%int(ppm)
+    if quiet == True:
+        command += ' --quiet'
+    if rate is not None:
+        command += ' --rate %i'%int(rate)
+    if buffer_size is not None:
+        command += ' --buffer-size %i'%int(buffer_size)
+    if strict_time is not None:
+        command += ' --strict-time %i'%int(strict_time)
+    if time is not None:
+        command += ' --time %s'%str(time)
+    if window is not None:
+        command += ' --window %i'%int(window)
+    if ignore_rest == True:
+        command += ' --ignore_rest'
+
+    print(command)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return output, error
+
+
+if __name__ == "__main__":
+    #Should append the unix time here so that it doesn't overwrite. 
+    if len(sys.argv) > 1:
+        outfile = sys.argv[1]
+    else:
+        outfile = 'capture'
+
+
+    while True:
+        call_time = time.time()
+
+        print('Submitting rtl_power_fftw')
+
+        outfile_new = outfile + '_' + str(call_time).replace('.','p')
+
+        output, error = rtlPowerFFTW(baseline=None, \
+                        bins=512, \
+                        buffers=None, \
+                        continuous=False, \
+                        device=None, \
+                        elapsed='30s', \
+                        freq=[30e6, 100e6], \
+                        gain=350, \
+                        linear=False, \
+                        matrix=outfile_new, \
+                        n=None, \
+                        overlap=None, \
+                        ppm=None, \
+                        quiet=True, \
+                        rate=None, \
+                        buffer_size=None, \
+                        strict_time=None, \
+                        time='1s', \
+                        window=None, \
+                        ignore_rest=False)
+
+
+        os.system('touch %s.ready'%(outfile_new))
+        print('Finished rtl_power_fftw and created .ready file.')
+        print(output)
+        print(error)
--- scripts/file_transfer.py
+++ scripts/file_transfer.py
@@ -16,32 +16,57 @@ if __name__ == "__main__":
     input_dir = '/home/nuphase/BEACON-SDR/captures/' #On Beagle (Where the script should be run.
     output_dir = '/home/dsouthall/BEACON-SDR/captures/' #On Sundog
     wait_time = 10 #s
-    extensions = ['*.bin','*.met']
+    extensions = ['*.ready'] #Only want it to transfer files once the .met file has been made, i.e. once it is complete.
     call_time = time.time()
     
     ssh = SSHClient()
     ssh.load_system_host_keys()
     ssh.connect('sundog.uchicago.edu',username='dsouthall')
     
-    # SCPCLient takes a paramiko transport and progress callback as its arguments.
-    with SCPClient(ssh.get_transport(), progress=progress) as scp:
-        while True:
-            files = []
-            for ext in extensions:
-                files.extend(glob.glob(input_dir + ext))
-            for f in files:
-                f = f.replace('\\','/') # To handle windows paths.
-                t = os.path.getmtime(f)
-                if t >= call_time:
-                    print('Transferring file modified since last call and appending unix time to name:')
-                    f_new = f.replace('.','_' + str(call_time).replace('.','p') + '.')
-                    f_new = output_dir + f_new.split('/')[-1]
-                    print(f_new)
-                    #os.rename(f,f_new) #Might want to do an scp if doing between pcs?
-                    scp.put(f, f_new)
-                    print('\n')
-                    os.remove(f)
-                    print('%s removed from local storage.\n'%f)
-            call_time = time.time()
-            time.sleep(wait_time)
-        scp.close()
+
+    while True:
+        files = []
+        for ext in extensions:
+            files.extend(glob.glob(input_dir + ext))
+        for f in files:
+            f = f.replace('\\','/') # To handle windows paths.
+            t = os.path.getmtime(f)
+            if t >= call_time:
+                # SCPCLient takes a paramiko transport and progress callback as its arguments.
+                # Might want to look into a keep alive if I only want to ssh once and not every call.
+                with SCPClient(ssh.get_transport(), progress=progress) as scp:
+                    #ready file
+                    f_new = output_dir + f.split('/')[-1]
+                    try:
+                        print('Removing %s'%f)
+                        os.remove(f)
+                    except Exception as e:
+                        print('Failed deleting %s.'%f)
+                        print(e)
+                    #met file
+                    try:
+                        f_new = f_new.replace('.ready','.met')
+                        print('Transferring met file modified since last call')
+                        scp.put(f, f_new)
+                        print('\n')
+                        os.remove(f)
+                        print('%s removed from local storage.\n'%f)
+                    except Exception as e:
+                        print('Failed copying/deleting %s.'%f)
+                        print(e)
+
+                    #bin file
+                    try:
+                        f_new = f_new.replace('.met','.bin')
+                        print('Transferring bin file modified since last call')
+                        scp.put(f, f_new)
+                        print('\n')
+                        os.remove(f)
+                        print('%s removed from local storage.\n'%f)
+                    except Exception as e:
+                        print('Failed copying/deleting %s.'%f)
+                        print(e)
+
+                scp.close()
+        call_time = time.time()
+        time.sleep(wait_time)
